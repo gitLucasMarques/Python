ea9ad45bd7d318bcbeb21aa3eec8e8b5421d4139
Lucas Marques <82738348+gitLucasMarques@users.noreply.github.com>
2025-10-07 19:56:58 -0300

    Delete ToDoList directory

--- ToDoList/database/connection.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-MONGO_DETAILS = "mongodb://localhost:27017"
-client = MongoClient(MONGO_DETAILS)
-
-database = client.todo_database
-task_collection = database.get_collection("tasks")
